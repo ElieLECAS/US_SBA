7af256bf6542d9c02fa5bfdb8acca57fffe29e9b
Elie LECAS <elielecas00@gmail.com>
2024-02-27 12:14:51 +0100

    api app

---
+++ app/main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class PredictApi(models.Model):
+    State = models.CharField(max_length=100, null=False, blank=False)
+    Zip = models.IntegerField()
+    BankState = models.CharField(max_length=100, null=False, blank=False)
+    ApprovalFY = models.IntegerField()
+    Term = models.IntegerField()
+    GrAppv = models.IntegerField()
+    SBA_Appv = models.IntegerField()
+
+

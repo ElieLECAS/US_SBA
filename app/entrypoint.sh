8f4b6d98fd2337a9aac11ed752bb5aaf3ca5bb79
Elie LECAS <elielecas00@gmail.com>
2024-03-13 11:07:32 +0100

    Azure finalised

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    echo "En attente de Postgres..."
+    sleep 1
+done
+
 echo "Effectuer les migrations"
 python manage.py makemigrations && python manage.py migrate
 
@@ -7,9 +12,9 @@ python manage.py collectstatic --no-input
 
 echo "Lancer le serveur"
 
-gunicorn sba_app.wsgi:application --workers=4 --timeout 120 --bind=0.0.0.0:8001
+gunicorn sba_app.wsgi:application --workers=4 --timeout 120 --bind=0.0.0.0:80
 
-# python manage.py runserver 0.0.0.0:8001
+# python manage.py runserver 0.0.0.0:80
 
 # >>> from django.conf import settings
 # >>> settings.DATABASES
